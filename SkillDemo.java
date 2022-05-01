e4001ca3aeb4a3700055d1df1ed6cf0ca4501934
bsalvania <bsalvania@ucsd.edu>
2022-05-01 16:34:06 -0700

    Added test file for skill demo

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,4 +1,7 @@
 public class SkillDemo {
 
+    public int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
 
 }
---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester {
+    
+    @Test
+    public void testMultiplication(){
+        assertEquals(10, SkillDemo.multiplication(2, 4));
+    }
+}
